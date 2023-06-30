605f1e6f655208adacafff83186cd31e6593a1f4
celso soares <celsosoares@sapo.mz>
2023-06-30 10:52:23 +0200

    Added the css module

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './layout.module.css';
 
 export const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 };
